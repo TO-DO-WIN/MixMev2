ddc96ccd869298a1c8ab060226f52f2ad0553724
mattMilton <matthewmilton@msn.com>
2018-11-22 21:02:38 -0600

    add rate review activity

---
+++ app/src/main/java/com/to_do_win/mixme_v2/UI/RateReviewActivity.java
@@ -0,0 +1,15 @@
+package com.to_do_win.mixme_v2.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.to_do_win.mixme_v2.R;
+
+public class RateReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_review);
+    }
+}
